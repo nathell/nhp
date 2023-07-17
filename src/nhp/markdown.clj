4fc15ea4c8923bc586c06e8a9b177ca18cd326d7
Daniel Janus <dj@danieljanus.pl>
2023-07-17 11:01:32 +0200

    Reinstate footnotes
    They're not clickable, but at least they're there.

--- src/nhp/markdown.clj
+++ src/nhp/markdown.clj
@@ -5,20 +5,34 @@
     [nextjournal.markdown.transform :as md.transform]
     [nhp.highlight :as highlight]))
 
+(defn render-footnote [ctx {:as node :keys [ref label]}]
+  (let [[tag par & content] (md.transform/into-markup [:div.footnote] ctx node)
+        [par-tag & par-content] par]
+    (into [tag
+           (into [par-tag [:sup.footnote-ref {:data-ref ref} (inc ref)] " "]
+                 par-content)]
+          content)))
+
 (def hiccup-renderers
   (merge md.transform/default-hiccup-renderers
-         {:plain (partial md.transform/into-markup [:span])
-          :code (fn [ctx {:keys [text content language] :as node}]
-                  (if (and language (seq language))
-                    [:pre
-                     (into [:code {:class (str "hljs " language)}]
-                           (keep (partial md.transform/->hiccup (assoc ctx ::md.transform/parent node)))
-                           [{:type :text
-                             :text (-> content first :text (highlight/highlight language))}])]
-                    (md.transform/into-markup [:pre.viewer-code.not-prose] ctx node)))}))
+         {:plain    (partial md.transform/into-markup [:span])
+          :footnote render-footnote
+          :code     (fn [ctx {:keys [text content language] :as node}]
+                      (if (and language (seq language))
+                        [:pre
+                         (into [:code {:class (str "hljs " language)}]
+                               (keep (partial md.transform/->hiccup (assoc ctx ::md.transform/parent node)))
+                               [{:type :text
+                                 :text (-> content first :text (highlight/highlight language))}])]
+                        (md.transform/into-markup [:pre.viewer-code.not-prose] ctx node)))}))
 
-(defn ->hiccup [content]
-  (md.transform/->hiccup hiccup-renderers content))
+(defn ->hiccup [{:keys [footnotes] :as content}]
+  (cond->
+      (md.transform/->hiccup hiccup-renderers content)
+    (seq footnotes) (into [[:hr]])
+    (seq footnotes)
+    (into
+     (map (partial md.transform/->hiccup hiccup-renderers) footnotes))))
 
 (def parse md/parse)
 
@@ -30,4 +44,6 @@
   (let [counts (map (comp count md.transform/->text) content)
         cumulative-counts (reductions + counts)
         num-paragraphs (inc (count (take-while #(< % min-content-length) cumulative-counts)))]
-    (update node :content (partial take num-paragraphs))))
+    (-> node
+        (update :content (partial take num-paragraphs))
+        (assoc :footnotes []))))
