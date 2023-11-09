dc57d7fffebd474cc1472f242eb7c5a7aff476b9
Daniel Janus <dj@danieljanus.pl>
2023-11-09 22:47:50 +0100

    Tweaks to Markdown rendering
    - Fix inline HTML rendering. This makes <sub> tags within
      paragraphs behave.
    - Render the image title as an alt attribute (in addition to
      title).

--- src/nhp/markdown.clj
+++ src/nhp/markdown.clj
@@ -1,9 +1,18 @@
 (ns nhp.markdown
   (:require
+    [hiccup.util :refer [raw-string]]
     [nextjournal.markdown :as md]
     [nextjournal.markdown.parser :as md.parser]
     [nextjournal.markdown.transform :as md.transform]
     [nhp.highlight :as highlight]))
+
+(defn render-image [{:as ctx ::keys [parent]} {:as node :keys [attrs]}]
+  (let [{:keys [title]} attrs]
+    (if (= :paragraph (:type parent))
+      [:img.inline attrs]
+      [:figure.image
+       [:img (cond-> attrs title (assoc :alt title))]
+       (md.transform/into-markup [:figcaption] ctx node)])))
 
 (defn render-footnote [ctx {:as node :keys [ref label]}]
   (let [[tag par & content] (md.transform/into-markup [:div.footnote] ctx node)
@@ -15,7 +24,9 @@
 
 (def hiccup-renderers
   (merge md.transform/default-hiccup-renderers
-         {:plain    (partial md.transform/into-markup [:span])
+         {:image    render-image
+          :raw      (fn [_ {:keys [text]}] (raw-string text))
+          :plain    (partial md.transform/into-markup [:span])
           :footnote render-footnote
           :code     (fn [ctx {:keys [text content language] :as node}]
                       (if (and language (seq language))
@@ -39,7 +50,10 @@
 
 ;; https://github.com/nextjournal/markdown/issues/7
 (defmethod md.parser/apply-token "html_block" [doc {inlined-html :content}]
-  (md.parser/push-node doc {:type :text :text inlined-html}))
+  (md.parser/push-node doc {:type :raw :text inlined-html}))
+
+(defmethod md.parser/apply-token "html_inline" [doc {inlined-html :content}]
+  (md.parser/push-node doc {:type :raw :text inlined-html}))
 
 (defn trim-content [min-content-length {:keys [content] :as node}]
   (let [counts (map (comp count md.transform/->text) content)
