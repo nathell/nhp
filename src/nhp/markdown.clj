9865868bcb5984084771a7cdf38ff887f1fcc018
Daniel Janus <daniel.janus@neuromoddevices.com>
2026-06-30 10:40:56 +0200

    Update to JVM 25
    Bump GraalVM and nextjournal/markdown to their most current versions,
    and massage the code a bit to account for changes in the latter.

--- src/nhp/markdown.clj
+++ src/nhp/markdown.clj
@@ -1,9 +1,7 @@
 (ns nhp.markdown
   (:require
-    [hiccup.util :refer [raw-string]]
+    [hiccup2.core :as hiccup]
     [nextjournal.markdown :as md]
-    [nextjournal.markdown.parser :as md.parser]
-    [nextjournal.markdown.transform :as md.transform]
     [nhp.highlight :as highlight]))
 
 (defn render-image [{:as ctx ::keys [parent]} {:as node :keys [attrs]}]
@@ -12,10 +10,10 @@
       [:img.inline attrs]
       [:figure.image
        [:img (cond-> attrs title (assoc :alt title))]
-       (md.transform/into-markup [:figcaption] ctx node)])))
+       (md/into-hiccup [:figcaption] ctx node)])))
 
 (defn render-footnote [ctx {:as node :keys [ref label]}]
-  (let [[tag par & content] (md.transform/into-markup [:div.footnote] ctx node)
+  (let [[tag par & content] (md/into-hiccup [:div.footnote] ctx node)
         [par-tag & par-content] par]
     (into [tag
            (into [par-tag [:sup.footnote-ref {:data-ref ref} (inc ref)] " "]
@@ -23,40 +21,34 @@
           content)))
 
 (def hiccup-renderers
-  (merge md.transform/default-hiccup-renderers
-         {:image    render-image
-          :raw      (fn [_ {:keys [text]}] (raw-string text))
-          :plain    (partial md.transform/into-markup [:span])
-          :footnote render-footnote
-          :code     (fn [ctx {:keys [text content language] :as node}]
-                      (if (and language (seq language))
-                        [:pre
-                         (into [:code {:class (str "hljs " language)}]
-                               (keep (partial md.transform/->hiccup (assoc ctx ::md.transform/parent node)))
-                               [{:type :text
-                                 :text (-> content first :text (highlight/highlight language))}])]
-                        [:pre
-                         (md.transform/into-markup [:code] ctx node)]))}))
+  (merge md/default-hiccup-renderers
+         {:image       render-image
+          :html-inline (comp hiccup/raw md/node->text)
+          :html-block  (comp hiccup/raw md/node->text)
+          :plain       (partial md/into-hiccup [:span])
+          :footnote    render-footnote
+          :code        (fn [ctx {:keys [text content language] :as node}]
+                         (if (and language (seq language))
+                           [:pre
+                            (into [:code {:class (str "hljs " language)}]
+                                  (keep (partial md/->hiccup (assoc ctx :nextjournal.markdown.transform/parent node)))
+                                  [{:type :text
+                                    :text (-> content first :text (highlight/highlight language))}])]
+                           [:pre
+                            (md/into-hiccup [:code] ctx node)]))}))
 
 (defn ->hiccup [{:keys [footnotes] :as content}]
   (cond->
-      (md.transform/->hiccup hiccup-renderers content)
+      (md/->hiccup hiccup-renderers content)
     (seq footnotes) (into [[:hr]])
     (seq footnotes)
     (into
-     (map (partial md.transform/->hiccup hiccup-renderers) footnotes))))
+     (map (partial md/->hiccup hiccup-renderers) footnotes))))
 
 (def parse md/parse)
 
-;; https://github.com/nextjournal/markdown/issues/7
-(defmethod md.parser/apply-token "html_block" [doc {inlined-html :content}]
-  (md.parser/push-node doc {:type :raw :text inlined-html}))
-
-(defmethod md.parser/apply-token "html_inline" [doc {inlined-html :content}]
-  (md.parser/push-node doc {:type :raw :text inlined-html}))
-
 (defn trim-content [min-content-length {:keys [content] :as node}]
-  (let [counts (map (comp count md.transform/->text) content)
+  (let [counts (map (comp count md/node->text) content)
         cumulative-counts (reductions + counts)
         num-paragraphs (inc (count (take-while #(< % min-content-length) cumulative-counts)))]
     (-> node
